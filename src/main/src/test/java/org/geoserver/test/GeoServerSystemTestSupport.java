7743fa078d76aae3a61b0b0a89c1bffc6ae641b6
Dennis Kao <ulysseskao@gmail.com>
2013-04-26 00:43:07 +0800

    for org.xml.sax.SAXParseException: Invalid byte 1 of 1-byte UTF-8 sequence.     	at org.apache.xerces.parsers.DOMParser.parse(Unknown Source)     	at org.apache.xerces.jaxp.DocumentBuilderImpl.parse(Unknown Source)     	at javax.xml.parsers.DocumentBuilder.parse(DocumentBuilder.java:124)     	at org.geoserver.test.GeoServerSystemTestSupport.dom(GeoServerSystemTestSupport.java:1288)

--- src/main/src/test/java/org/geoserver/test/GeoServerSystemTestSupport.java
+++ src/main/src/test/java/org/geoserver/test/GeoServerSystemTestSupport.java
@@ -1118,6 +1118,7 @@ public class GeoServerSystemTestSupport extends GeoServerBaseTestSupport<SystemT
         request.setContentType(contentType);
         request.setBodyContent(body);
         request.setHeader("Content-type",  contentType );
+        request.setCharacterEncoding("UTF-8");
 
         return dispatch(request);
     }

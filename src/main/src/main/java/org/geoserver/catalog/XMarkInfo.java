467e610ba640e6ad4226fedae6c0bd3aebebb0be
Dennis Kao <ulysseskao@gmail.com>
2013-05-06 10:53:15 +0800

    GEODMMS-60 設備符號

---
+++ src/main/src/main/java/org/geoserver/catalog/XMarkInfo.java
@@ -0,0 +1,11 @@
+package org.geoserver.catalog;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ulysseskao
+ * Date: 13/5/6
+ * Time: 上午10:33
+ * To change this template use File | Settings | File Templates.
+ */
+public interface XMarkInfo {
+}
